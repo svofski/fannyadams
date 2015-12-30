98bd39131dcf494c1b79aeb2dc417f5377a0d018
svo <svofski@gmail.com>
2015-12-31 02:12:14 +0300

    tabs

--- src/fannyadams/power.c
+++ src/fannyadams/power.c
@@ -15,96 +15,96 @@
 #define MIN_DEADTIME (0000 + 32)
 
 static const uint8_t power_table[8] = {
-	0300 + 0, 		// minimal power: ~ 50% duty
-	0200 + 48,
-	0200 + 32,
-	0200 + 16,
-	0200 + 0,
-	0100 + 32,
-	0100 + 0,
-	MIN_DEADTIME};  // maximum power
+    0300 + 0,       // minimal power: ~ 50% duty
+    0200 + 48,
+    0200 + 32,
+    0200 + 16,
+    0200 + 0,
+    0100 + 32,
+    0100 + 0,
+    MIN_DEADTIME};  // maximum power
 
 static volatile uint8_t dead_set = MIN_DEADTIME;
 
 
-#define PWM_FREQ	100000ul
-#define PERIOD 		(168000000ul/PWM_FREQ/2 - 1)
-#define HALFPERIOD 	(PERIOD/2)
+#define PWM_FREQ    100000ul
+#define PERIOD      (168000000ul/PWM_FREQ/2 - 1)
+#define HALFPERIOD  (PERIOD/2)
 
 static void Reconfigure(void) {
-	// no prescaler (168MHz clock)
-	timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_CENTER_1, TIM_CR1_DIR_UP);
-	timer_continuous_mode(TIM1); 						// keeps on running
+    // no prescaler (168MHz clock)
+    timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_CENTER_1, TIM_CR1_DIR_UP);
+    timer_continuous_mode(TIM1);                        // keeps on running
 
-	timer_disable_break(TIM1); 							// break function not used
+    timer_disable_break(TIM1);                          // break function not used
 
-	timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_FROZEN); 	// OC1 used for timing base
-	timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_PWM2); 	// OC3 is the PWM channel
+    timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_FROZEN);   // OC1 used for timing base
+    timer_set_oc_mode(TIM1, TIM_OC3, TIM_OCM_PWM2);     // OC3 is the PWM channel
 
-	timer_set_oc_value(TIM1, TIM_OC1, 0); 				// OC1 is frozen
-	timer_set_oc_value(TIM1, TIM_OC3, HALFPERIOD); 		// OC3 is the main pwm, always 50% for simmetry
-	timer_set_period(TIM1, PERIOD); 					// sets the frequency
- 	timer_set_deadtime(TIM1, dead_set); 				// dead-time used for protection and duty cycle
+    timer_set_oc_value(TIM1, TIM_OC1, 0);               // OC1 is frozen
+    timer_set_oc_value(TIM1, TIM_OC3, HALFPERIOD);      // OC3 is the main pwm, always 50% for simmetry
+    timer_set_period(TIM1, PERIOD);                     // sets the frequency
+    timer_set_deadtime(TIM1, dead_set);                 // dead-time used for protection and duty cycle
 
-	timer_set_oc_polarity_high(TIM1, TIM_OC3); 			// _|     |_________  high when active
-	timer_set_oc_idle_state_unset(TIM1, TIM_OC3); 		// ____ zero when MOE = 0 ___
+    timer_set_oc_polarity_high(TIM1, TIM_OC3);          // _|     |_________  high when active
+    timer_set_oc_idle_state_unset(TIM1, TIM_OC3);       // ____ zero when MOE = 0 ___
 
-	timer_set_oc_polarity_high(TIM1, TIM_OC3N);         // _________|     |__ high when active
-	timer_set_oc_idle_state_unset(TIM1, TIM_OC3N);		// ____ zero when MOE = 0 ___
-	
-	timer_enable_oc_output(TIM1, TIM_OC3); 				// enable OC3 
-	timer_enable_oc_output(TIM1, TIM_OC3N);				// and complementary OC3N
+    timer_set_oc_polarity_high(TIM1, TIM_OC3N);         // _________|     |__ high when active
+    timer_set_oc_idle_state_unset(TIM1, TIM_OC3N);      // ____ zero when MOE = 0 ___
+    
+    timer_enable_oc_output(TIM1, TIM_OC3);              // enable OC3 
+    timer_enable_oc_output(TIM1, TIM_OC3N);             // and complementary OC3N
 
-	timer_enable_break_automatic_output(TIM1);			// AOE: outputs are disabled but will be enabled 
-														// on the next update event
+    timer_enable_break_automatic_output(TIM1);          // AOE: outputs are disabled but will be enabled 
+                                                        // on the next update event
 }
 
 void Power_Setup(void) {
-	// GPIO Pins
-	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_periph_clock_enable(RCC_GPIOB);
+    // GPIO Pins
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_GPIOB);
 
     gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10);
     gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO1);
 
-	gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO10);
-	gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO1);
+    gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO10);
+    gpio_set_output_options(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO1);
 
     gpio_set_af(GPIOA, GPIO_AF1, GPIO10); // PA.10 = TIM1_CH3
     gpio_set_af(GPIOB, GPIO_AF1, GPIO1);  // PB.1  = TIM1_CH3N
 
     // Enable TIM1 power, reset and reconfigure
-	rcc_periph_clock_enable(RCC_TIM1);
-	timer_reset(TIM1);
-	Reconfigure();
+    rcc_periph_clock_enable(RCC_TIM1);
+    timer_reset(TIM1);
+    Reconfigure();
 
-	// interrupt for power adjustments
-	nvic_enable_irq(NVIC_TIM1_UP_TIM10_IRQ);
+    // interrupt for power adjustments
+    nvic_enable_irq(NVIC_TIM1_UP_TIM10_IRQ);
 }
 
 void Power_Start(void) {
-	//timer_enable_break_main_output(TIM1); // not necessary, because:
-	timer_enable_counter(TIM1);	 			// AOE setting will set MOE on first update event
+    //timer_enable_break_main_output(TIM1); // not necessary, because:
+    timer_enable_counter(TIM1);             // AOE setting will set MOE on first update event
 }
 
 void Power_Stop(void) {
-	timer_disable_break_main_output(TIM1); 	// force idle state on outputs (unset, low)
-	timer_disable_counter(TIM1); 			// stop
+    timer_disable_break_main_output(TIM1);  // force idle state on outputs (unset, low)
+    timer_disable_counter(TIM1);            // stop
 }
 
 void Power_Scale(int factor) {
-	uint8_t f = factor < 0 ? 0 : factor > 7 ? 7 : (uint8_t) factor;
-	dead_set = power_table[f];
-	xprintf("Power_Scale(%d) deadtime=%d\n\r", factor, dead_set);
-	timer_clear_flag(TIM1, TIM_SR_UIF);
-	timer_enable_irq(TIM1, TIM_DIER_UIE);	
+    uint8_t f = factor < 0 ? 0 : factor > 7 ? 7 : (uint8_t) factor;
+    dead_set = power_table[f];
+    xprintf("Power_Scale(%d) deadtime=%d\n\r", factor, dead_set);
+    timer_clear_flag(TIM1, TIM_SR_UIF);
+    timer_enable_irq(TIM1, TIM_DIER_UIE);   
 }
 
 void tim1_up_tim10_isr(void) {
-	timer_clear_flag(TIM1, TIM_SR_UIF); 	// clear interrupt flag
-	timer_disable_break_main_output(TIM1);  // set outputs to idle mode (both channels low)
-	timer_reset(TIM1); 						// completely reset the timer, otherwise deadtime cannot be set
-	Reconfigure(); 							// set up everything
-	timer_enable_break_main_output(TIM1); 	// force master output enable (MOE) without waiting for auto
-	timer_enable_counter(TIM1); 			// start the timer again
+    timer_clear_flag(TIM1, TIM_SR_UIF);     // clear interrupt flag
+    timer_disable_break_main_output(TIM1);  // set outputs to idle mode (both channels low)
+    timer_reset(TIM1);                      // completely reset the timer, otherwise deadtime cannot be set
+    Reconfigure();                          // set up everything
+    timer_enable_break_main_output(TIM1);   // force master output enable (MOE) without waiting for auto
+    timer_enable_counter(TIM1);             // start the timer again
 }
